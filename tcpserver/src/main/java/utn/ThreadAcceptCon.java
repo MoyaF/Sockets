18ef98c7695b5a647293e5b95f010d40d01aa1ea
Fausto Moya <37676075+MoyaF@users.noreply.github.com>
2019-06-20 23:49:50 -0300

    Format

--- tcpserver/src/main/java/utn/ThreadAcceptCon.java
+++ tcpserver/src/main/java/utn/ThreadAcceptCon.java
@@ -7,35 +7,33 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ThreadAcceptCon extends Thread{
+public class ThreadAcceptCon extends Thread {
 
-    TCPServer ms;
-    ServerSocket ss;
-    Socket s;
-    ThreadClient tc;
-    List<ThreadClient> threadClients;
+    private TCPServer ms;
+    private ServerSocket ss;
+    private Socket s;
+    private ThreadClient tc;
+    private List<ThreadClient> threadClients;
 
-    public ThreadAcceptCon(TCPServer ms, ServerSocket ss)
-    {
+    public ThreadAcceptCon(TCPServer tcps, ServerSocket ss) {
         super();
-        this.ms = ms;
+        ms = tcps;
         this.ss = ss;
         this.threadClients = new ArrayList<>();
     }
+
     @Override
     public void run() {
-        while(!ss.isClosed()){
-            try{
-                s= ss.accept();
-                System.out.println("Conectado con "+s.getLocalAddress().getHostAddress());
-                tc=new ThreadClient(s);
+        while (!ss.isClosed()) {
+            try {
+                s = ss.accept();
+                System.out.println("Conectado con " + s.getLocalAddress().getHostAddress());
+                tc = new ThreadClient(s);
                 ms.addObserver(tc);
-                tc.setName("Conexion con"+ s.getLocalAddress());
+                tc.setName("Conexion con" + s.getLocalAddress());
                 tc.start();
                 threadClients.add(tc);
-            }
-
-            catch(SocketException e){
+            } catch (SocketException e) {
                 System.out.println("Server socket cerrado con exito");
 
             } catch (IOException e) {
@@ -44,9 +42,8 @@ public class ThreadAcceptCon extends Thread{
         }
     }
 
-    public void close(){
-        for(ThreadClient t : threadClients)
-        {
+    public void close() {
+        for (ThreadClient t : threadClients) {
             t.close();
         }
 
